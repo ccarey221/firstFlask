14c8d164dd65dfbad7c079d92c1191516a2ac483
Conor Carey <careyc@union.edu>
2017-02-08 15:45:31 -0800

    quotes added

---
+++ quotes.py
@@ -0,0 +1,13 @@
+def funny_quotes():
+    return [{
+        "author" : "Elbert Hubbard",
+        "quote" : "Do not take life too seriously. You will never get out of it alive."
+    },
+    {
+        "author" : "Mark Twain",
+        "quote" : "Get your facts first, then you can distort them as you please."
+    },
+    {
+        "author" : "Benjamin Franklin",
+        "quote" : "Wine is constant proof that god loves us and loves to see us happy"
+    }]
